b14ccad2af4e6fc0b9baa380c91b652f1e35721e
Anh Quynh Nguyen <quynhhanhh@Anhs-MacBook-Pro.local>
2021-07-05 13:57:01 -0400

    Add Router

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Router>
+          <Switch>
+              <Route exact path="/">
+                  <App />
+              </Route>
+          </Switch>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
